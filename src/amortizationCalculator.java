6b193c920078d5733f9a960652b1b4b2fa55635f
amitkulkarni <amit3992@gatech.edu>
2016-08-21 17:36:27 -0700

    Initial checkin of amortization calc tool

---
+++ src/amortizationCalculator.java
@@ -0,0 +1,15 @@
+
+public class amortizationCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Main method to start the program
+		
+		// call set up object
+		// Call method to take input from user
+		
+
+	}
+
+}
