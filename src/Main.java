bf5a95e001e94c7079c02285f9d871da3866a6a5
amitkulkarni <amitkulkarni@10.0.0.8>
2016-08-21 17:45:10 -0700

    Removed old file. Checked in Main

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Main method to start the program
+		// Program flow is as follows
+				// call set up object
+				// Call method to take input from user
+				// Get total loan amount
+				// Get APR from credit score
+				// Calculate total payments
+
+	}
+
+}
